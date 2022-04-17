11ddaf473b6f903659c91fce65491b37bc49febf
Tamires <tamires.manhaes@ifba.edu.br>
2022-04-16 22:08:39 -0300

    chore: importing type e formating

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,33 +1,29 @@
 import client from '../client/index';
-import { CreateTodo } from '../types';
+import { CreateTodo, ITodo } from '../types';
 
-const getAllTodos = async () => {
+const getAllTodos = async (): Promise<ITodo[]> => {
   const tasks = (await client.get('/todos')).data;
 
   return tasks;
-}
+};
 
 const toDoDone = async (id: string, isDone: Boolean) => {
-  const response = (await client.put(`/todo/done/${id}`, { is_done: isDone })).data;
+  const response = (await client.put(`/todo/done/${id}`, { is_done: isDone }))
+    .data;
 
   return response;
-}
+};
 
 const createToDo = async (todo: CreateTodo) => {
   const response = (await client.post('/todo', todo)).data;
 
   return response;
-}
+};
 
 const deleteToDo = async (taskId: string) => {
   const response = (await client.delete(`/todo/${taskId}`)).data;
 
   return response;
-}
+};
 
-export {
-  getAllTodos,
-  createToDo,
-  deleteToDo,
-  toDoDone
-}
+export { getAllTodos, createToDo, deleteToDo, toDoDone };
