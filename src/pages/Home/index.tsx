135079e1c23c6ce6d3704b85b318b9a6390d50f0
Tamires <tamires.manhaes@ifba.edu.br>
2022-04-16 22:08:22 -0300

    chore: refactoring Home page file and calls

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,149 +1,78 @@
-import React, { FormEvent, useEffect, useState } from 'react';
-import { Container, Content, Form, ItemTask, TasksList, TaskBox, ActionsBox, Loader } from './style';
-import { BiTrash, BiCheckCircle, BiXCircle } from 'react-icons/bi';
-import { DeleteTask,  ILoader, TaskItemProps } from '../../types';
-import { getAllTodos, createToDo, deleteToDo, toDoDone} from '../../services/index';
-
-const Loading: React.FC<ILoader> = ({ isLoading }) => {
-  return (
-    <>
-      {
-        isLoading ? (
-          <Loader>
-            <div className="loader">Loading...</div>
-          </Loader>
-        )  : (
-          <span></span>
-        )
-      }
-    </>
-  );
-}
-
-const TaskItem: React.FC<TaskItemProps> = ({ id, task, is_done, deleteTodo }) => {
-  const isDone = is_done;
-
-  const setDoneTask = async (id: string, isDone: Boolean) => {
-    const taskDone = isDone === true ? false : true;
-    await toDoDone(id, taskDone);
-    window.location.reload();
-  }
-
-  return (
-    <ItemTask>
-      <TaskBox>
-        <span className={`${is_done ? 'isDone' : 'notDone'}`}>{task}</span>
-      </TaskBox>
-
-      <ActionsBox>
-        <button type="button" onClick={() => setDoneTask(id, isDone)} className={`${is_done ? 'isDone' : 'notDone'}`} title={`${is_done ? 'done' : 'not done'}`}>
-          {
-           isDone === true ? (
-            <BiXCircle size={25} color="var(--red)" />
-           ) : (
-            <BiCheckCircle size={25} color="var(--green)" />
-          )
-          }
-        </button>
-
-        <button type="button" onClick={() => deleteTodo(id)} className="deleteButton">
-          <BiTrash size={25} color="#fff" />
-        </button>
-      </ActionsBox>
-    </ItemTask>
-  );
-}
+import React, { FormEvent, useContext, useEffect, useState } from 'react';
+import Loading from '../../components/Loading';
+import TaskItem from '../../components/TaskItem';
+import { Container, Content, Form, TasksList } from './style';
+import { ActionsBox } from '../../components/TaskItem/styles';
+import { ToDoContext } from '../../context/ToDo';
+import { LoadingContext } from '../../context/Loading';
 
 const Home = () => {
-  const [tasks, setTasks] = useState<TaskItemProps[]>([]);
-  const [value, setValue] = useState<string>("");
-  const [isLoading, setIsLoading] = useState(true);
+  const { todos, addTodo, deleteTodo, getAlltodos } = useContext(ToDoContext);
+  const { isLoading, setIsLoading } = useContext(LoadingContext);
+  const [value, setValue] = useState<string>('');
 
-  const addForm = async (event: FormEvent) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if(value){
-      const newTask = {
-        task: value,
-        is_done: false
-      }
-
+    if (value.length === 1) {
+      alert('adicione uma task!');
+    } else {
       setIsLoading(true);
-      const newTaskResponse = await createToDo(newTask);
-      setTasks([...tasks, newTaskResponse]);
+      addTodo(value);
+      setValue(' ');
       setIsLoading(false);
     }
-    
-  }
-
-  const deleteTask: DeleteTask = async (id: string) => {
-    try {
-      setIsLoading(true);
-      await deleteToDo(id);
-      setTasks(tasks.filter(task => task.id !== id));
-      setValue(` `);
-      setIsLoading(false);
-    }catch(e){
-      alert('error!');
-    }
-  }
+  };
 
   useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      const localTasks = await getAllTodos();
-      setTasks(localTasks);
-      setIsLoading(false);
-    })()
+    setIsLoading(true);
+    getAlltodos();
+    setIsLoading(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <Container>
       <Content>
         <h1>To-Do List</h1>
-        <Form onSubmit={addForm}>
-          <input 
-            type="text" 
-            required  
-            placeholder="Add Task" 
-            onChange={e => setValue(e.target.value)} 
+        <Form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            required
+            placeholder="Add Task"
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
           />
           <ActionsBox>
-            <button 
-              type="submit" 
-              className="add"
-            >
+            <button type="submit" className="add">
               Adicionar
             </button>
 
-            <button 
-              type="reset" 
-              onClick={e => setValue(" ")} 
+            <button
+              type="reset"
+              onClick={(e) => setValue(' ')}
               className="cancel"
             >
               Cancelar
             </button>
           </ActionsBox>
-
         </Form>
         <TasksList>
-          {
-            tasks && tasks.map(task => (
-              <TaskItem 
-                key={task.id} 
-                id={task.id} 
-                task={task.task} 
-                is_done={task.is_done} 
-                deleteTodo={deleteTask} 
+          {todos &&
+            todos.map((task) => (
+              <TaskItem
+                key={task.id}
+                id={task.id}
+                task={task.task}
+                is_done={task.is_done}
+                deleteTodo={deleteTodo}
               />
-            ))
-          }
+            ))}
         </TasksList>
 
-        <Loading isLoading={isLoading}/>
-
+        <Loading isLoading={isLoading} />
       </Content>
     </Container>
-  )
-}
+  );
+};
 
 export default Home;
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -18,6 +18,7 @@ export const Content = styled.nav`
   h1 {
     font-size: var(--font-large);
     font-weight: 900;
+    text-align: center;
   }
 `;
 
@@ -35,17 +36,6 @@ export const Form = styled.form`
   }
 `;
 
-export const ItemTask = styled.li`
-  background: #fff;
-  color: #000;
-  border-bottom: 1px solid var(--gray);
-  padding: 5px 10px;
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
 export const TasksList = styled.ul`
   margin: 20px 0 0;
   padding: 10px 10px 0 10px;
@@ -53,169 +43,4 @@ export const TasksList = styled.ul`
 
   max-height: 520px;
   overflow-y: auto;
-`;
-
-export const TaskBox = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: var(--font-normal);
-
-  > input{
-    width: 20px;
-    height: 20px;
-    margin-right: 10px;
-  }
-
-  > .isDone {
-    text-decoration: line-through
-  }
-
-  > .notDone{
-    text-decoration: none;
-  }
-`;
-
-export const ActionsBox = styled.div`
-  > button {
-    border: none;
-    margin: 0 5px;
-    border-radius: 4px;
-    cursor: pointer;
-    transition: all 250ms ease;
-  }
-  
-  > .add , .cancel {
-    height: 35px;
-    width: 90px;
-    padding: 5px 10px;
-  }
-  
-  > .deleteButton , .editButton {
-    padding: 5px;
-  }
-
-  > .deleteButton, .cancel {
-    color: #fff;
-    background: var(--red);
-    border: 1px solid var(--red);
-
-    &:hover {  
-      background: #fff; 
-      color: var(--red);
-      border: 1px solid var(--red);
-      > svg { color: var(--red) !important; }
-    }
-  }
-
-  > .editButton, .add {
-    background: var(--green);
-    color: #fff;
-    border: 1px solid var(--green);
-    
-    &:hover { 
-      background: #fff;
-      color: var(--green);
-      border: 1px solid var(--green);
-      > svg { color: var(--green) !important; }
-    }
-  }
-
-  > .isDone, .notDone {
-    border: 1px solid rgba(0,0,0, .1);
-    height: 40px;
-  }
-
-  > .isDone {
-    background: var(--white);
-    color: var(--green);
-  }
-
-  > .notDone {
-    background: var(--white);
-    color: var(--red)
-  }
-`;
-
-export const Loader = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background: rgba(255, 255, 255, .7);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  .loader,
-  .loader:before,
-  .loader:after {
-    border-radius: 50%;
-  }
-
-  .loader {
-    color: var(--light-blue);
-    font-size: 11px;
-    text-indent: -99999em;
-    margin: 55px auto;
-    position: relative;
-    width: 10em;
-    height: 10em;
-    box-shadow: inset 0 0 0 1em;
-    -webkit-transform: translateZ(0);
-    -ms-transform: translateZ(0);
-    transform: translateZ(0);
-  }
-
-  .loader:before,
-  .loader:after {
-    position: absolute;
-    content: '';
-  }
-
-  .loader:before {
-    width: 5.2em;
-    height: 10.2em;
-    background: #fff;
-    border-radius: 10.2em 0 0 10.2em;
-    top: -0.1em;
-    left: -0.1em;
-    -webkit-transform-origin: 5.1em 5.1em;
-    transform-origin: 5.1em 5.1em;
-    -webkit-animation: load2 2s infinite ease 1.5s;
-    animation: load2 2s infinite ease 1.5s;
-  }
-  .loader:after {
-    width: 5.2em;
-    height: 10.2em;
-    background: transparent;
-    border-radius: 0 10.2em 10.2em 0;
-    top: -0.1em;
-    left: 4.9em;
-    -webkit-transform-origin: 0.1em 5.1em;
-    transform-origin: 0.1em 5.1em;
-    -webkit-animation: load2 2s infinite ease;
-    animation: load2 2s infinite ease;
-  }
-
-  @-webkit-keyframes load2 {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
-  @keyframes load2 {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
 `;
